868a18d4b12570ee6617ecb3f59c18ac485f45de
Nelson Rivas <nelson.rivas.ucv+code@gmail.com>
2023-01-28 19:55:09 -0400

    Adding scripts, favicon, and image folder

---
+++ src/scripts/index.js
@@ -0,0 +1,12 @@
+document.querySelector('#hamburguer').addEventListener('click', () => {
+  console.log('menu clicked')
+  // document.querySelector('#menu').classList.toggle('w-3/4')
+  document.querySelector('#menu').classList.toggle('w-96')
+  document.querySelector('#title').classList.toggle('-z-50')
+  document.querySelector('#aboutme').classList.toggle('-z-50')
+  document.querySelector('#menu').classList.toggle('hidden')
+  // md screen
+  document.querySelector('#menu').classList.toggle('md:w-72')
+  document.querySelector('#title').classList.toggle('md:-z-0')
+  document.querySelector('#aboutme').classList.toggle('md:-z-0')
+})
